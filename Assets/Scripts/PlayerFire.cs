5f984e7fb0fa1480d5db1581cc3cf25344ff0ba9
Shaheer Ahmed <128972759+SherryMaster@users.noreply.github.com>
2025-09-06 21:12:35 +0500

    Add target point movement to PlayerFire
    Introduces targetPoint and targetDistance fields to PlayerFire. Implements MoveTargetPoint to update the target point's position based on mouse input and camera projection. Updates SampleScene to reference the new targetPoint and targetDistance.

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -5,6 +5,8 @@ public class PlayerFire : MonoBehaviour
 {
     [SerializeField] ParticleSystem[] LaserGuns;
     [SerializeField] RectTransform crosshair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance = 250f;
 
     bool isFiring;
 
@@ -17,6 +19,7 @@ public class PlayerFire : MonoBehaviour
     {
         ProcessFiring();
         MoveCrosshair();
+        MoveTargetPoint();
     }
 
     public void OnFire(InputValue value)
@@ -37,6 +40,12 @@ public class PlayerFire : MonoBehaviour
     {
         Vector2 mousePosition = Mouse.current.position.ReadValue();
         crosshair.position = mousePosition;
+    }
+
+    void MoveTargetPoint()
+    {
+        Vector3 targetPointPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
     }
 
 }
