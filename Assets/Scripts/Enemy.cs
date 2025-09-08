92deee7d4f2e578568b5bfe6660e27021cff6c65
Shaheer Ahmed <128972759+SherryMaster@users.noreply.github.com>
2025-09-08 14:47:46 +0500

    Add Enemy script with particle collision handling
    Introduced Enemy.cs, which destroys the enemy GameObject upon particle collision. Updated Laser prefab to send collision messages and integrated the Enemy script into the SampleScene for proper behavior.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnParticleCollision(GameObject other)
+    {
+        Destroy(this.gameObject);
+    }
+}
